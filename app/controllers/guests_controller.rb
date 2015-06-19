9aa943a612365991a5570507340141e4e6456749
Joyce <jyu21@ncsu.edu>
2015-06-19 15:10:18 -0400

    first commit

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,72 @@
+class GuestsController < ApplicationController
+  before_filter :authorize, :only => [:edit, :update, :destroy]
+  before_action :all_guests, only: [:index, :create, :update, :destroy]
+  before_action :set_guest, only: [:show, :edit, :update, :destroy]
+
+
+  # GET /guests/new
+  def new
+    @guest = Guest.new
+  end
+
+  # GET /guests
+  # GET /guests.json 
+
+  def option
+
+    @choice = params[:choice].to_s
+    @fontsize = params[:fontsize].to_s 
+    @fontfam = params[:fontfam].to_s
+
+      @guests = Guest.all
+      respond_to do |format|
+        format.html
+        format.pdf do 
+          pdf = GuestsPdf.new(@guests, @choice, @fontfam, @fontsize)
+          send_data pdf.render, filename: "print.pdf",
+                      type: "application/pdf",
+                      disposition: "inline" 
+
+        end 
+      end 
+    
+  end
+
+  def create
+    @guest = Guest.create(guest_params)
+    session[:guest_ids] ||= []
+    session[:guest_ids] << @guest.id
+  end
+
+  def update 
+    @guest.update(guest_params) 
+  end 
+
+  def destroy 
+    @guest.destroy 
+  end 
+
+  def import 
+    Guest.import(params[:file])
+    redirect_to root_url, notice: "Guestlist imported."
+  end 
+
+  private
+    def authorize 
+      unless session[:guest_ids] && session[guest_ids].include?(params[:id].to_i)
+        redirect_to root_path
+      end 
+    end 
+    def all_guests
+      @guests = Guest.all
+    end 
+    # Use callbacks to share common setup or constraints between actions.
+    def set_guest
+      @guest = Guest.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def guest_params
+      params.require(:guest).permit(:name, :table_num)
+    end
+end
---
+++ app/pdfs/Guests_pdf.rb
@@ -0,0 +1,177 @@
+class GuestsPdf < Prawn::Document
+	def initialize(guests, choice, fontfam, fontsize)
+		super(top_margin:40)
+		if session[:guest_ids] && session[:guest_ids].include?(guest.id)
+			@guests = guests
+		end 
+		
+		# @even = @guests.values_at(* guests.each_index.select{|d| d.even?})
+		# @odd = @guests.values_at(* guests.each_index.select{|d| d.odd?})
+		@choice = choice
+		@fontfam = fontfam
+		@fontsize = fontsize
+		#Registering font families 
+		if fontfam.to_i == 1
+		 font_families.update("font_standard" => {
+		 	:normal => "#{Rails.root}/app/assets/fonts/AlexBrush.ttf"
+		 	})
+		elsif fontfam.to_i == 2
+			font_families.update("font_standard" => {
+		 	:normal => "#{Rails.root}/app/assets/fonts/Crushed.ttf"
+		 	})
+		elsif fontfam.to_i == 3
+			font_families.update("font_standard" => {
+		 	:normal => "#{Rails.root}/app/assets/fonts/HomemadeApple.ttf"
+		 	})
+		elsif fontfam.to_i == 4
+			font_families.update("font_standard" => {
+		 	:normal => "#{Rails.root}/app/assets/fonts/Magnolia Light.ttf"
+		 	})
+		else 
+		 	font_families.update("font_standard" => {
+		 	:normal => "#{Rails.root}/app/assets/fonts/theodista_decally-webfont.ttf"
+		 	})
+		end 
+	
+		print_names
+
+	end
+
+	def print_names
+		length = @guests.size
+		image_name = "app/assets/images/pla" + @choice + ".png"
+		gap = @fontsize.to_i
+		name_size =  @fontsize.to_i
+		table_size = @fontsize.to_i-5
+
+		font("font_standard")
+		if length > 4 
+			1.upto(length/4.floor) do |i| 
+				define_grid(:columns => 2, :rows => 4, :gutter => 2)
+				grid(1,0).bounding_box do
+					image image_name, :fit => [bounds.width, bounds.height], 
+					:postion => :center, :vposition => :center
+					float do
+						bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+							text @guests[4*i-4].table_num.to_s, :valign => :center, :align => :center, :size => name_size
+							move_down gap
+							text @guests[4*i-4].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+						end 
+					end 
+				end 
+				grid(1,1).bounding_box do
+					image image_name, :fit => [bounds.width, bounds.height], 
+					:postion => :center, :vposition => :center
+					float do
+						bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+							text @guests[4*i-3].name, :valign => :center, :align => :center, :size => name_size
+							move_down gap
+							text @guests[4*i-3].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+						end 
+					end 
+				end 
+				grid(3,0).bounding_box do
+					image image_name, :fit => [bounds.width, bounds.height], 
+					:postion => :center, :vposition => :center
+					float do
+						bounding_box([bounds.width/3-10,bounds.height*1], :width => 100) do 
+							text @guests[4*i-2].name, :valign => :center, :align => :center, :size => name_size
+							move_down gap
+							text @guests[4*i-2].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+						end 
+					end 
+				end 
+				grid(3,1).bounding_box do
+					image image_name, :fit => [bounds.width, bounds.height], 
+					:postion => :center, :vposition => :center
+					float do
+						bounding_box([bounds.width/3-10,bounds.height*1], :width => 100) do 
+							text @guests[4*i-1].name, :valign => :center, :align => :center, :size => name_size
+							move_down gap
+							text @guests[4*i-1].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+						end 
+					end 
+				end 
+				start_new_page
+			end 
+		end 
+
+		if length%4 != 0 
+			addition = length-length%4
+			addition.upto(length) do |i| 
+			define_grid(:columns => 2, :rows => 4, :gutter => 2)
+				if length%4 == 1
+					grid(1,0).bounding_box do
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+								text @guests[length-length%4].table_num.to_s, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end
+				elsif length%4 == 2 
+					grid(1,0).bounding_box do
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+								text @guests[length-length%4].name, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end 
+					grid(1,1).bounding_box do
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+								text @guests[length-length%4+1].name, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4+1].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end
+				else 
+					grid(1,0).bounding_box do
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+								text @guests[length-length%4].name, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end 
+					grid(1,1).bounding_box do
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*3], :width => 100) do 
+								text @guests[length-length%4+1].name, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4+1].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end 
+					grid(3,0).bounding_box do 
+						image image_name, :fit => [bounds.width, bounds.height], 
+						:postion => :center, :vposition => :center
+						float do
+							bounding_box([bounds.width/3-10,bounds.height*1], :width => 100) do 
+								text @guests[length-length%4+2].name, :valign => :center, :align => :center, :size => name_size
+								move_down gap
+								text @guests[length-length%4+2].table_num.to_s, :valign => :center, :align => :center, :size => table_size
+							end 
+						end 
+					end
+				end 
+			end 
+		end 
+	end 
+end 
+
